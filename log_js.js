498030ee62556e86959d90089fc4e934da68f862
Purev <purevpurev042@gmail.com>
2025-05-18 18:17:04 +0800

    test test

--- log_js.js
+++ log_js.js
@@ -1,16 +1,21 @@
-emailjs.init("b5fimnsFG6GN7r1tv"); // ← PUBLIC KEY
+  emailjs.init("b5fimnsFG6GN7r1tv");
 
-document.getElementById("registerForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+    document.getElementById("registerForm").addEventListener("submit", function (e) {
+      e.preventDefault();
 
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  const phone = document.getElementById("phone").value;
-  const email = document.getElementById("email").value;
+      const templateParams = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+      };
 
-  const templateParams = {
-    firstName,
-    lastName,
-    phone,
-    email,
-  }:
+      emailjs.send("service_5se6cfm", "template_2ksog6f", templateParams)
+        .then((response) => {
+          alert("Амжилттай илгээгдлээ!");
+          document.getElementById("registerForm").reset();
+        })
+        .catch((error) => {
+          alert("Алдаа гарлаа: " + error.text);
+        });
+    });
